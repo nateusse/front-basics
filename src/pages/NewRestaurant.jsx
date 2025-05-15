2be68c90ea8456b0b1c256a6ddde11aee8832552
nateusse <natyeusse@gmail.com>
2025-05-15 09:21:38 -0400

    feature: list restaurants from json

---
+++ src/pages/NewRestaurant.jsx
@@ -0,0 +1,10 @@
+function NewRestaurant() {
+  return (
+    <div>
+      <h1>New Restaurant</h1>
+      <p>Form to create a new restaurant goes here.</p>
+    </div>
+  );
+}
+
+export default NewRestaurant;
